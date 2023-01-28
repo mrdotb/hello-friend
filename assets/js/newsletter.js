73105019f08ad9778cc908ca37efee9e4e6abf91
mrdotb <mrdotb@protonmail.com>
2023-01-28 06:49:20 +0100

    cleanup newsletter worker call

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -4,7 +4,7 @@ newsletterList.forEach($newsletter => {
   const $form = $newsletter.querySelector("form");
   const $email = $newsletter.querySelector("input")
   const $result = $newsletter.querySelector("p.result")
-  const url = DEV_MODE ? "https://mailerlite.mrdotb.workers.dev/" : "/mailerlite";
+  const url = DEV_MODE ? "https://mrdotb-workers.mrdotb.workers.dev/api/subscribe" : "/api/subscribe";
 
   $form.addEventListener("submit", event => {
     event.preventDefault();
@@ -25,7 +25,7 @@ newsletterList.forEach($newsletter => {
         $result.textContent = "Thanks for subscribing, please check your inbox for a confirmation email.";
         setTimeout(() => $result.textContent = "", 4000)
       } else {
-        $result.textContent = "Could not subscribe";
+        $result.textContent = response.error;
       }
     })
   })
